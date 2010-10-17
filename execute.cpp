d891d34db40c301be0d1c2715d09df3f64374f9f
chesarin <vargasces@gmail.com>
2010-10-16 22:57:13 -0400

    Adding input file and adding input file facilities

--- execute.cpp
+++ execute.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 using namespace std;
 int main(){
-  cout << "Testing Traveling Salesman" << endl;
+  ifstream ifs ("input.txt");
+  cout << "Opening the file with input nodes" << endl;
+  while (ifs.good())
+    cout << ifs.get() << endl;
   return 0;
 }
