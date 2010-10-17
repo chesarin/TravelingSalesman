73444b181118d32cfce1fc797dca2aec8aad2632
chesarin <vargasces@gmail.com>
2010-10-17 13:43:27 -0400

    Adding Node.h

---
+++ Node.h
@@ -0,0 +1,11 @@
+#ifndef NODE_H_
+#define NODE_H_
+class Node{
+ public:
+  int id,num_level,g;
+  double f,h;
+  Node* father;
+  Node(){ father=NULL; };
+  Node(int i, Node* f=0):id(i),father(f){};
+};
+#endif
