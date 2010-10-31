283acbcf1ff606cdb2ebcb91a4f02c936eed3226
chesarin <vargasces@gmail.com>
2010-10-31 15:18:00 -0400

    incr_child(), children added. We need to keep track how many kids if any the nodes have.

--- Node.h
+++ Node.h
@@ -6,16 +6,18 @@
 #define NODE_H_
 class Node{
  public:
-  int id,num_level,g,cost;
+  int id,num_level,g,cost,children;
   double f,h;
   bool present;
   Node* father;
- Node():present(false){ father=0; };
- Node(int i, Node* f=0):id(i),father(f){};
+ Node():present(false){ father=0; children=0;};
+ Node(int i, Node* f=0):id(i),father(f){children=0;};
   void set_id(int i){id=i;};
   void set_cost(int i){cost=i;};
   void set_presence(){present=true;};
+  void calculate_f(){ f = cost + h; };
   bool is_present(){ return present;};
   void set_father(Node* f){ father=f;};
+  void incr_child(){ children+=1; };
 };
 #endif
