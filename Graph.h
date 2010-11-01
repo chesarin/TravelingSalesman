59626211378d641b7564a59b0ab139a0c508fa83
chesarin <vargasces@gmail.com>
2010-10-31 22:09:37 -0400

    functions calculate_h, calculate_children_f, explore, calculate_size and look_up added in order to transverse the graph and find the shortest path.

--- Graph.h
+++ Graph.h
@@ -4,22 +4,30 @@
  */
 #ifndef GRAPH_H_
 #define GRAPH_H_
-#define MAX 6
+#define MAX 20
 #include "Node.h"
 #include <vector>
 #include <iostream>
+#include <list>
 using namespace std;
 class Graph{
  private:
   int cities_visited;
   int size;
   Node matrix[MAX][MAX];
+  list<int> openlist;
+  list<int> closedlist;
  public:
   Graph();
   void create(const vector <int>& in);
   void print();
-  void find_lowest(int node_num);
-  void update_h(int node_num, int nodes_left);
-  void explore(int begin);
+  int find_lowest(int node_num);
+  void update_children_h(int node_num);
+  void calculate_h(int node_num, int nodes_left);
+  void calculate_children_f(int node_num);
+  void explore(int total_nodes, int start_node);
+  void expand(int node_num);
+  void calculate_size();
+  bool look_up(int node_num);
 };
 #endif
