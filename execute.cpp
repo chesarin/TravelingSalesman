19551c6d434fdce3520c7cb951955f095d33231a
chesarin <vargasces@gmail.com>
2010-10-31 22:06:03 -0400

    Testing explore functions, this functions will try to traverse the graph and find the shortest path.

--- execute.cpp
+++ execute.cpp
@@ -41,9 +41,10 @@ void graph(){
   cout << "in graph method" << endl;
   test.create(nodes);
   test.print();
-  test.find_lowest(1);
-  test.find_lowest(2);
-  test.find_lowest(3);
-  test.find_lowest(4);
-  test.update_h(1,2);
+  test.explore(6,1);
+  // test.find_lowest(1);
+  // test.find_lowest(2);
+  // test.find_lowest(3);
+  // test.find_lowest(4);
+  // test.calculate_h(1,6);
 }
